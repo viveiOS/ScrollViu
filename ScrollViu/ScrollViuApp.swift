b5e5f05dcd91471bb5aa2b01a2e279e403922cdf
ThumViu <appdevelopp2121@gmail.com>
2021-10-12 00:42:59 -0600

    Initial Commit

---
+++ ScrollViu/ScrollViuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViuApp.swift
+//  ScrollViu
+//
+//  Created by Abraham De Leon on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
